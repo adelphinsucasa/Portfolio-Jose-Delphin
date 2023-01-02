c72cad10954da35c4efc69314453c7275c3aafba
josedelphin <dalejandro.005@gmail.com>
2023-01-02 17:12:37 -0400

    Add Transition and Animation

--- hamburguer.js
+++ hamburguer.js
@@ -5,19 +5,20 @@ let hamburguer_link = document.querySelectorAll('.hamburguer-link');
 
 
 btnClose.addEventListener('click', function (event) {
-  divHamburguer.classList.add('invisible');
+  divHamburguer.classList.add('run-out');
+  divHamburguer.classList.remove('run');
 });
 
 iconHamburguer.addEventListener('click', () => {
-  divHamburguer.classList.add('visible');
-  divHamburguer.classList.remove('invisible');
+  divHamburguer.classList.add('run');
+  divHamburguer.classList.remove('run-out');
 });
 
 for (let elem of hamburguer_link) {
 
   elem.addEventListener('click', () => {
-    divHamburguer.classList.add('invisible');
-    divHamburguer.classList.remove('visible');
+    divHamburguer.classList.add('run-out');
+    divHamburguer.classList.remove('run');
   })
 
 }
