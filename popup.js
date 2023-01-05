fbcf81d6f2081f9d4fd5657a42f98530b5f67ef4
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-01-05 16:43:09 -0400

    Add modal logic

--- popup.js
+++ popup.js
@@ -1,64 +1,64 @@
 const recentWorksObj = [
   {
-    id: "project1",
+    id: 'project1',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
   },
   {
-    id: "project2",
+    id: 'project2',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
   },
   {
-    id: "project3",
+    id: 'project3',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
   },
   {
-    id: "project4",
+    id: 'project4',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
   },
   {
-    id: "project5",
+    id: 'project5',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
   },
   {
-    id: "project6",
+    id: 'project6',
     name: 'Keeping track of hundreds of components',
     technologies: ['Ruby on Rails', 'css', 'Javascript', 'html'],
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!",
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint doloremque ducimus reiciendis dignissimos asperiores quis maiores atque, impedit delectus id laboriosam repellendus sit sed placeat, velit tenetur! Enim, saepe aliquid praesentium possimus facere qui corrupti velit maxime dolorum reprehenderit ullam minus, assumenda repudiandae aspernatur cupiditate cum esse commodi nam perspiciatis!',
     imageURL_desktop: './assets/images/popup/desktop-img.png',
     imageURL_mobile: './assets/images/popup/mobile-img.png',
-    liveDemo: "https://chelobotix.github.io/Portafolio-Website/",
-    repository: "https://github.com/chelobotix/Portafolio-Website"
-  }
+    liveDemo: 'https://chelobotix.github.io/Portafolio-Website/',
+    repository: 'https://github.com/chelobotix/Portafolio-Website',
+  },
 ];
 
 // Heading Section
@@ -114,3 +114,65 @@ for (let i = 0; i < recentWorksObj.length; i += 1) {
   cardRow.appendChild(article);
 }
 sectionWorks.appendChild(cardRow);
+
+// Modal
+
+const btnSeeProjects = document.querySelectorAll('.btn_see_projects');
+const btnClose2 = document.querySelector('.close');
+
+function createModal(index) {
+  const imgModal = document.querySelector('img.modal-project');
+  const h3Modal = document.querySelector('h3.modal-project');
+  const btn1Modal = document.querySelectorAll('.modal-btn1');
+  const btn2Modal = document.querySelectorAll('.modal-btn2');
+  const ulModal = document.querySelector('ul.modal-project');
+  const pModal = document.querySelector('p.modal-project');
+
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    imgModal.src = recentWorksObj[index].imageURL_mobile;
+  } else {
+    imgModal.src = recentWorksObj[index].imageURL_desktop;
+  }
+
+  h3Modal.textContent = recentWorksObj[index].name;
+
+  btn1Modal.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      window.location.href = recentWorksObj[index].liveDemo;
+    });
+  });
+
+  btn2Modal.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      window.location.href = recentWorksObj[index].repository;
+    });
+  });
+
+  recentWorksObj[index].technologies.forEach((tech) => {
+    const newLi = document.createElement('li');
+    newLi.textContent = tech;
+    ulModal.appendChild(newLi);
+  });
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    ulModal.removeChild(ulModal.lastChild);
+  } else {
+    btnClose2.textContent = 'xddddddddddddd';
+  }
+
+  pModal.textContent = recentWorksObj[index].description;
+}
+
+function showModal() {
+  const blur = document.querySelector('.blur');
+  const body = document.querySelector('body');
+
+  body.style.overflow = 'hidden';
+  blur.classList.remove('invisible');
+}
+
+btnSeeProjects.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    createModal(index);
+    showModal();
+  });
+});
